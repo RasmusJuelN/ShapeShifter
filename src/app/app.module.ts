b65c4e9559d99cb577161a902ea603538637df4c
RasmusJN <rasmusn686@hotmail.com>
2023-11-01 12:51:23 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BackgroundComponent } from './Components/background/background.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { LandingpageComponent } from './Components/landingpage/landingpage.component';
+import { LoginComponent } from './Components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackgroundComponent,
+    HeaderComponent,
+    LandingpageComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
